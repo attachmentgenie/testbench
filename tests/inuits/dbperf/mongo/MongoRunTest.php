2847aa116d1db979d5728f8cca392a82a009b804
Bram Vogelaar <attachmentgenie@inuits.eu>
2014-12-22 18:41:29 +0100

    initial import

---
+++ tests/inuits/dbperf/mongo/MongoRunTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace inuits\dbperf\mongo;
+
+class MongoRunTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRunTests()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
